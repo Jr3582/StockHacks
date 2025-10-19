17030e4ab351d69f05661bdca8ce86292250555c
Jr3582 <jr3582@drexel.edu>
2025-10-19 16:34:27 -0400

    Update stock-hacks/components/Header.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- stock-hacks/components/Header.tsx
+++ stock-hacks/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = async ( { user }: { user: User }) => {
         <header className="sticky top-0 header">
             <div className="container header-wrapper">
                 <Link href="/">
-                    <Image src="/assets/icons/logo.svg" alt="StockHacks logo" width={140} height={32} className="h-8 w-auto curser-pointer"></Image>
+<Image src="/assets/icons/logo.svg" alt="StockHacks logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
                 </Link>
                 <nav className="hidden sm:block">
                     <NavItems initialStocks={initialStocks}/>
